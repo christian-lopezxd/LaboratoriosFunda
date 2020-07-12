ef03060153a3b8c29e60e2e547502cde30e2326b
christian-lopezxd <00179320@uca.edu.sv>
2020-07-12 15:58:39 -0600

    finalizando Funda

--- Fundamentos de Programacion/PROYECTO FINAL/Proyectofinal.cpp
+++ Fundamentos de Programacion/PROYECTO FINAL/Proyectofinal.cpp
@@ -1,52 +1,132 @@
+//-+-+-+-+-+-+-+-+-+-+-Facturacion De Productos-+-+-+-+-+-+-+-+-+-+-
 #include <iostream>
+#include <string.h>
+#include <math.h>
 #include <conio.h>
-#include <iomanip>
-
 using namespace std;
 
-const int longCad = 20;
-//Declarando Estructura
+//Struct
+const int Cad=20;
+
 struct costoPorArticulo{
-char nombreArticul[longCad + 1];
-int cantidad;
-float precio;
-float costoPorArticulo;
+    char nombreArticulo[Cad+1];
+    int cantidad;
+    float precio;
+    float costoPorArticulo;
 };
-//Prototipos
-int Lecturadedatos(costoPorArticulo articulos[], int n);
-void calculandocosto(costoPorArticulo p);
-void Desplegando(costoPorArticulo p);
-int main(){
-    int n;
-    cout<<"BIENVENIDO AL PROGRAMA"<<endl;
-    cout<<"ingrese la cantidad de articulos a incluir"<<endl;
-    cin>>n;
-    cin.ignore(100, '/n');
-    cout<<"pongame 10"<<endl;
-    costoPorArticulo articulos[n];
 
-int Lecturadedatos(costoPorArticulo articulos[], int n);
+//FUNCIONES
+void leerDatos(costoPorArticulo art[]);
+void calcularCosto(costoPorArticulo art[], int cant);
+void desplegarContenido(costoPorArticulo art[], int cantidad);
+float valorTotalCompra(costoPorArticulo art[], int cantidad);
 
-    
-    
-    
-    
+int cont = 0;
 
+//LLAMAR FUNCIONES
+int main(void){
+    int cantidadDeProductos;
+    
+    cout<<"----FACTURA DE COMPRA---- \n \n";
+    cout<<"ingrese la cantidad de productos de diferente clase: "<<endl;
+    cin >> cantidadDeProductos;
+    
+    
+    //arreglo donde se guardan los artículos
+    costoPorArticulo articulos[cantidadDeProductos];
+
+    //ciclo for para n productos
+    for(int i=0; i<cantidadDeProductos; i++){
+        leerDatos(articulos);
+        cout<<"\n";
+    }
+    
+    //llamando funciones
+    calcularCosto(articulos, cantidadDeProductos);
+    
+    cout<<"\n----------------------------------------- \n \n";
+
+    //mostrar todos los artículos
+    desplegarContenido(articulos, cantidadDeProductos);
+    valorTotalCompra(articulos, cantidadDeProductos); 
+    
 
     getch();
     return 0;
 }
 
-int Lecturadedatos(costoPorArticulo articulos[], int n){
+//LEYENDO LOS DATOS
 
-    for (int i = 0; i < n; i++)
-    {
+void leerDatos(costoPorArticulo art[]){
+    //Variables
+    char nombreArticulo[21];
+    float precio;
+    int cantidad;
     
-        cout<<"ingrese el nombre del articulo"<<endl;
-    cin.getline(articulos[i].nombreArticul,longCad);
-    cout<<"ingrese la cantidad a comprar de ese producto"<<endl;
-    cin>>articulos[i].cantidad;
-    cout<< "ingrese el precio del articulo"<<endl;
-    cin>> articulos[i].precio;
+    
+    cout<<"\nIngrese el nombre del articulo: "<<endl;
+    fflush(stdin);
+    cin.getline(nombreArticulo, 20, '\n');
+    cout<<"Ingrese la cantidad de elementos que va a llevar: "<<endl;
+    cin >> cantidad;
+    cout<<"Ingrese el precio del articulo: "<<endl;
+    cin >> precio;
+    cout<<"-----------------------------------------"<<endl;
+    
+    
+    costoPorArticulo costo;
+    
+    //Llenar los campos
+    for (int i = 0; i < 21; i++)
+    {
+        costo.nombreArticulo[i] = nombreArticulo[i];
     }
+    
+   
+    costo.cantidad = cantidad;
+    costo.precio = precio;
+    
+   
+    art[cont] = costo;
+    
+    
+    cont++;
+}
+
+//MULTIPLICANDO
+void calcularCosto(costoPorArticulo art[], int cant){
+    //Se recorre todo el arreglo
+    for(int i=0; i<cant; i++){
+       
+    art[i].costoPorArticulo = art[i].precio * art[i].cantidad;
+    }
+}
+
+//MOSTRANDO
+void desplegarContenido(costoPorArticulo art[], int cantidad){
+   
+    for(int i=0; i<cantidad; i++){
+    cout << "\n---- Articulo " << i+1 << " ----" << endl;    
+    cout << "Nombre: " << art[i].nombreArticulo << endl;
+    cout << "Cantidad: " << art[i].cantidad << endl;
+    cout << "Precio: $" << art[i].precio << endl;
+    cout << "Costo por articulo: $" << art[i].costoPorArticulo << endl;
+    }
+}
+
+float valorTotalCompra(costoPorArticulo art[], int cantidad){
+
+   float totalapagar;
+    for (int i = 0; i < cantidad; i++)
+    {
+        
+         totalapagar+= art[i].costoPorArticulo;
+
+        
+    }
+    cout<<endl<<endl;
+    
+    cout<<"|Total a Pagar | $"<<totalapagar<<"   |"<<endl;
+    
+
 }
